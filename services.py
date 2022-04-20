d3fe658a357734eb2cb198d2cf05a7be990121a8
AndyDem <>
2022-04-20 13:08:14 +0300

    minor code changes

--- services.py
+++ services.py
@@ -4,57 +4,70 @@ from config import OPEN_WEATHER_TOKEN
 
 
 def get_weather(city):
-
-    code_to_smile = {
-        "Clear": "\U00002600",
-        "Clouds": "\U00002601",
-        "Rain": "\U00002614",
-        "Drizzle": "\U00002614",
-        "Thunderstorm": "\U000026A1",
-        "Snow": "\U0001F328",
-        "Mist": "\U0001F32B"
-    }
-
     try:
-        r = requests.get(
-            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPEN_WEATHER_TOKEN}&units=metric'
-        )
-        data = r.json()
-
-        city = data['name']
-        today = datetime.fromtimestamp(data['dt']).date().strftime('%d-%m-%Y')
-        current_temperature = data['main']['temp']
-        current_feels_like = data['main']['feels_like']
-        current_humidity = data['main']['humidity']
-        current_wind_speed = data['wind']['speed']
-        today_sunrise = datetime.fromtimestamp(
-            data['sys']['sunrise'])
-        today_sunset = datetime.fromtimestamp(
-            data['sys']['sunset'])
-        today_daylength = today_sunset - today_sunrise
-        current_weather = data['weather'][0]['main']
-        if current_weather in code_to_smile:
-            current_weather += code_to_smile[current_weather]
-
-        current_weather = f'Weather in {city} for today {today} is:\n' \
-            f'{current_weather}\n' \
-            f'Current temparature: {current_temperature}°C, which feels like {current_feels_like} °C\n' \
-            f'Current humidity: {current_humidity}%\n' \
-            f'Current wind speed: {current_wind_speed} m/s\n' \
-            f'Sunrise time: {today_sunrise.time()}\n' \
-            f'Sunset time: {today_sunset.time()}\n' \
-            f'Day length is {today_daylength}'
-
+        raw_weather_data = get_raw_weather(city)
+        current_weather = get_message(raw_weather_data)
         return current_weather
 
     except Exception as e:
         print(e)
         return 'Something happened, check the city or try again, please'
 
+def get_raw_weather(city):
+    r = requests.get(
+        f'https://api.openweathermap.org/raw_weather_data/2.5/weather?q={city}&appid={OPEN_WEATHER_TOKEN}&units=metric'
+    )
+    return r.json()
+
+def get_message(raw_weather_data):
+    city = raw_weather_data['name']
+    today = datetime.fromtimestamp(raw_weather_data['dt']).date().strftime('%d-%m-%Y')
+    current_temperature = int(raw_weather_data['main']['temp'])
+    current_feels_like = int(raw_weather_data['main']['feels_like'])
+    current_humidity = raw_weather_data['main']['humidity']
+    current_wind_speed = raw_weather_data['wind']['speed']
+    today_sunrise = datetime.fromtimestamp(
+        raw_weather_data['sys']['sunrise'])
+    today_sunset = datetime.fromtimestamp(
+        raw_weather_data['sys']['sunset'])
+    today_daylength = today_sunset - today_sunrise
+    current_weather = raw_weather_data['weather'][0]['description'].capitalize()
+
+    current_weather = add_emoji(current_weather)
+
+    message = f'Current weather in {city} for today {today} is:\n' \
+        f'{current_weather}\n' \
+        f'Temperature: {current_temperature}°C, feels like {current_feels_like}°C\n' \
+        f'Humidity: {current_humidity}%\n' \
+        f'Wind speed: {current_wind_speed} m/s\n' \
+        f'Sunrise time: {today_sunrise.time()}\n' \
+        f'Sunset time: {today_sunset.time()}\n' \
+        f'Day length is {today_daylength}'
+
+    return message
+
+def add_emoji(text:str):
+    emoji = {
+        "Clear": " \U00002600",
+        "Clouds": " \U00002601",
+        "Rain": " \U00002614",
+        "Drizzle": " \U00002614",
+        "Thunderstorm": " \U000026A1",
+        "Snow": " \U0001F328",
+        "Mist": " \U0001F32B"
+    }
+
+    for word in text.split():
+            if word in emoji:
+                text += emoji[word]
+                break
+
+    return text
+
 
 def main():
     city = input('City: ')
-    get_weather(city)
+    print(get_weather(city))
 
 
 if __name__ == '__main__':
